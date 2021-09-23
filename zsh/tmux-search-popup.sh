53eb6c02526e2aece64904271c38c80b95ca645c
Tomás Salazar <tomas@bitso.com>
2021-09-23 12:24:22 -0500

    Add search on files fzf, improve mappind IDEA

--- zsh/tmux-search-popup.sh
+++ zsh/tmux-search-popup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-history -r script_history
+history -r ~/.tmux_search_history
 set -o vi
 CMD=""
-echo "Search:"
-read -e CMD
+# echo -ne "\033[1;49;91m(Search up): \033[0m"
+read -e -p  "(Search up):" CMD
 history -s "$CMD"
-history -w script_history
+history -w ~/.tmux_search_history
 # tmux send-keys -t "$TMUX_PANE" -X search-forward "$CMD"
 tmux copy-mode \; send -X search-backward "$CMD"
