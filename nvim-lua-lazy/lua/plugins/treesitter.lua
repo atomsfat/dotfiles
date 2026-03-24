744ae54e682fa7d9ba883b1a884b356c49b86b57
atoms <70m4s@pm.me>
2026-03-23 23:47:46 -0600

    Update treesiiter conf

--- nvim-lua-lazy/lua/plugins/treesitter.lua
+++ nvim-lua-lazy/lua/plugins/treesitter.lua
@@ -1,102 +1,75 @@
+-- treesitter.lua
 return {
-  -- Treesitter is a new parser generator tool that we can
-  -- use in Neovim to power faster and more accurate
-  -- syntax highlighting.
-  {
-    "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
-    build = ":TSUpdate",
-    event = { "VeryLazy" },
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        config = function()
-          -- When in diff mode, we want to use the default
-          -- vim text objects c & C instead of the treesitter ones.
-          local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
-          local configs = require("nvim-treesitter.configs")
-          for name, fn in pairs(move) do
-            if name:find("goto") == 1 then
-              move[name] = function(q, ...)
-                if vim.wo.diff then
-                  local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-                  for key, query in pairs(config or {}) do
-                    if q == query and key:find("[%]%[][cC]") then
-                      vim.cmd("normal! " .. key)
-                      return
-                    end
-                  end
-                end
-                return fn(q, ...)
-              end
-            end
-          end
-        end,
-      },
-    },
-    cmd = { "TSUpdateSync" },
-    keys = {
-      { "<c-space>", desc = "Increment selection" },
-      { "<bs>", desc = "Decrement selection", mode = "x" },
-    },
-    ---@type TSConfig
-    opts = {
-      highlight = { enable = true, use_languaguetree = true },
-      indent = { enable = true },
-      ensure_installed = {
-        "bash",
-        "c",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-      textobjects = {
-        move = {
-          enable = true,
-          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-        },
-      },
-    },
-    ---@param opts TSConfig
-    config = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        ---@type table<string, boolean>
-        local added = {}
-        opts.ensure_installed = vim.tbl_filter(function(lang)
-          if added[lang] then
-            return false
-          end
-          added[lang] = true
-          return true
-        end, opts.ensure_installed)
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local ts = require "nvim-treesitter"
+    local parsers = {
+      "bash",
+      "comment",
+      "css",
+      "diff",
+      "dockerfile",
+      "elixir",
+      "git_config",
+      "gitcommit",
+      "gitignore",
+      "groovy",
+      "go",
+      "heex",
+      "hcl",
+      "html",
+      "http",
+      "java",
+      "javascript",
+      "jsdoc",
+      "json",
+      "json5",
+      "lua",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "rst",
+      "rust",
+      "scss",
+      "ssh_config",
+      "sql",
+      "terraform",
+      "typst",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    }
+
+    for _, parser in ipairs(parsers) do
+      ts.install(parser)
+    end
+
+    -- Not every tree-sitter parser is the same as the file type detected
+    -- So the patterns need to be registered more cleverly
+    local patterns = {}
+    for _, parser in ipairs(parsers) do
+      local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+      for _, pp in pairs(parser_patterns) do
+        table.insert(patterns, pp)
       end
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
+    end
+
+    vim.treesitter.language.register("groovy", "Jenkinsfile")
+    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    vim.wo[0][0].foldmethod = "expr"
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = patterns,
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+  end,
 }
